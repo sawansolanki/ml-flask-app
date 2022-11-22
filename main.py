1f542a2acfcedd91e708f60dfe321fcdd79fd5bf
sawan solanki <sawan.solanki@yash.com>
2022-11-22 11:38:39 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from unittest import result
+from flask import Flask , render_template , request
+import numpy as np
+import pickle
+
+model=pickle.load(open('titanic_model_lr_Sa1','rb'))
+
+app=Flask(__name__)
+
+@app.route ('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+
+def predict_survival():
+    pclass=int(request.form.get('pclass'))
+    sex=int(request.form.get('sex'))
+    age=int(request.form.get('age'))
+    fare=int(request.form.get('fare'))
+    
+    result = model.predict(np.array([pclass,sex,age,fare]).reshape(1,4))
+    
+    if result==1:
+        result="Alive"
+    elif result==0:
+        result="Dead"
+        
+    return str(result)
+    
+
+    
+    
+if __name__=='__main__':
+    app.run(debug=True)
