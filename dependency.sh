42ceb8d3a83bc14b0e17d7e04d834fb6a6e1ded0
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-19 17:19:46 +0530

    Update dependency.sh

--- dependency.sh
+++ dependency.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt-get update
-sudo apt-get install zip unzip
-unzip awscliv2.zip
-sudo ./aws/install
+#sudo apt-get install zip unzip
+sudo apt-get update less
+#unzip awscliv2.zip
+#sudo ./aws/install
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-echo y | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo service docker start
+# echo y | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# sudo service docker start
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
