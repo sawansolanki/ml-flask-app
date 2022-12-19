f422508b125e71edf6d8a7de4d4e3306edcc0088
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-19 15:14:36 +0530

    Update stack-script.sh

--- stack-script.sh
+++ stack-script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-export STACK_NAME=my-bucket-stacwewk
+export STACK_NAME=my-EKS-VPC-stack
 output=$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} 2>&1)
 
 if [ $? -ne 0 ]; then
   if echo ${output} | grep -q ValidationError; then
         echo 'creating stack....' ${STACK_NAME}
+        aws cloudformation deploy --template-file ${{env.ROOT_PATH}}/aws-vpc-for-eks.yaml --stack-name ${STACK_NAME}
   else
     >&2 echo "Error is -->" ${output}
   fi
