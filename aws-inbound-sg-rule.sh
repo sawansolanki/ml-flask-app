b9f5cbbe41e4dd8da002549b0f1ee228db32ef1e
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-20 13:51:23 +0530

    Update aws-inbound-sg-rule.sh

--- aws-inbound-sg-rule.sh
+++ aws-inbound-sg-rule.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-sg_id=sg-0c874a2a7d963fef0
+sg_id=sg-09d56c064f5936e41
 Protocol=tcp
-port=30479
+port=31479
 cidr=0.0.0.0/0
 
 output=$(aws ec2 authorize-security-group-ingress     --group-id $sg_id     --protocol $Protocol     --port $port     --cidr 0.0.0.0/0 2>&1)
 
 if [ $? -ne 0 ]; then
   if echo ${output} | grep -q InvalidPermission.Duplicate; then
-        echo 'sg inbound rule already exist'
-		
+        echo 'sg inbound rule for port $port already exist'
+
   else
-	aws ec2 authorize-security-group-ingress     --group-id $sg_id     --protocol $Protocol     --port $port     --cidr $cidr 2>&1
     >&2 echo "Error is -->" ${output}
   fi
 fi
