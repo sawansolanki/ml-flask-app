9ddacedf58c7dddb1f6a02e62d6fabcaffadf9b6
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-20 16:31:21 +0530

    Update dependency.sh

--- dependency.sh
+++ dependency.sh
@@ -8,8 +8,8 @@ sudo apt-get update less
 #sudo ./aws/install
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-# echo y | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-# sudo service docker start
+echo y | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo service docker start
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
