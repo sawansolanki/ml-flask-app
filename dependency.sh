1a321ca2fcc73323f402afc8298184653e873308
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-22 14:44:53 +0530

    Update dependency.sh

--- dependency.sh
+++ dependency.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
 sudo apt-get install ca-certificates
-
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` test"
-sudo apt update
-sudo apt install docker-ce
-
-
 #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt-get update
 #sudo apt-get install zip unzip
@@ -17,7 +9,7 @@ sudo apt-get install less
 #sudo ./aws/install
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-#echo y | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+#echo y | sudo apt-get install docker containerd.io docker-compose-plugin
 sudo service docker start
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
